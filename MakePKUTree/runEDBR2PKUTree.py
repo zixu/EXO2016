f8fc23ab9497113d376f4459a82858572d9ef1f6
zixu <zixu@cern.ch>
2016-01-11 01:12:50 -0600

    Make PKU Tree

---
+++ MakePKUTree/runEDBR2PKUTree.py
@@ -0,0 +1,67 @@
+import os
+import random
+import sys
+import ctypes
+import time
+
+import ROOT
+
+from optparse import OptionParser
+parser = OptionParser()
+
+parser.add_option('-b', action='store_true', dest='noX', default=False, help='no X11 windows')
+(options, args) = parser.parse_args()
+
+
+ROOT.gSystem.Load("EDBR2PKUTree_C.so")
+from ROOT import EDBR2PKUTree, TFile, TTree
+
+
+
+if __name__ == '__main__': 
+    #input_dir="./inputEDBRTree/"
+    #input_dir="./ntuple/"
+    #input_dir="/afs/cern.ch/user/q/quwang/work/RunII/EXOMET/CMSSW_7_3_0/src/ExoDiBosonResonances/EDBRTreeMaker/test/ntuple/"
+    #input_dir="/afs/cern.ch/user/h/huangh/work/samples_750/huang_root/"#"/afs/cern.ch/user/q/quwang/work/RunII/CMSSW_7_4_3/src/ExoDiBosonResonances/EDBRTreeMaker/test/15Spring_50ns/"
+    input_dir="./BG_signal_samples/"
+
+    infile=open("filelist.txt")
+
+    totalsamples=infile.readlines()
+
+    for sample in totalsamples:
+        if sample[0]!="#":
+
+            print "Loading ", sample
+            sampleconfig=sample.split(';')
+            samplename=sampleconfig[0]
+            sampleXS=sampleconfig[1]
+            sampleNum=sampleconfig[2]
+
+            samplename=samplename.replace(' ', '').replace('\n','').replace('/n','') 
+            sampleXS  =float(sampleXS.replace(' ', '').replace('\n','').replace('/n','') )
+            sampleNum =int(sampleNum.replace(' ', '').replace('\n','').replace('/n','') )
+
+            if (sampleXS-1.)==0: IsData=0
+            elif "WJetsToLNu_HT" in samplename: IsData=2 
+            else: IsData=1
+
+            print samplename
+            print sampleXS
+            print sampleNum 
+            print IsData
+
+            filein =TFile(input_dir+samplename);
+            dir1 = filein.Get("treeDumper");
+            treein = dir1.Get("EDBRCandidates")
+            print "Open Tree Done"
+
+            channel="el"
+            analyzer_el=EDBR2PKUTree(treein,channel+"_out_"+samplename);
+            analyzer_el.Loop(channel, sampleXS, sampleNum, IsData);
+            analyzer_el.endJob();
+
+            channel="mu"
+            analyzer_mu=EDBR2PKUTree(treein,channel+"_out_"+samplename);
+            analyzer_mu.Loop(channel, sampleXS, sampleNum, IsData);
+            analyzer_mu.endJob();
