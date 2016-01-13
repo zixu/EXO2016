e31b69e90e8589c123b667e580cb60cfa5478e58
zixu <zixu@cern.ch>
2016-01-12 21:31:55 -0600

    allbkg

--- MakePKUTree/setup_mu.sh
+++ MakePKUTree/setup_mu.sh
@@ -7,7 +7,7 @@ hadd -f mu_PKUTree_VV_xww.root mu_out_WWToLNuQQ_13TeV-powheg.root mu_out_WZTo1L1
 
 hadd -f  mu_PKUTree_WJetsPt180_xww.root  mu_out_WJetsToLNu_HT-100To200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8.root mu_out_WJetsToLNu_HT-200To400_TuneCUETP8M1_13TeV-madgraphMLM-pythia8.root mu_out_WJetsToLNu_HT-400To600_TuneCUETP8M1_13TeV-madgraphMLM-pythia8.root mu_out_WJetsToLNu_HT-600To800_TuneCUETP8M1_13TeV-madgraphMLM-pythia8.root mu_out_WJetsToLNu_HT-800To1200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8.root mu_out_WJetsToLNu_HT-1200To2500_TuneCUETP8M1_13TeV-madgraphMLM-pythia8.root mu_out_WJetsToLNu_HT-2500ToInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8.root
 
-#hadd -f mu_PKUTree_allBkg_xww.root  mu_PKUTree_SingleTop_xww.root mu_PKUTree_TTBARpowheg_xww.root mu_PKUTree_VV_xww.root mu_PKUTree_WJetsPt180_xww.root
+hadd -f mu_PKUTree_allBkg_xww.root  mu_PKUTree_SingleTop_xww.root mu_PKUTree_TTBARpowheg_xww.root mu_PKUTree_VV_xww.root mu_PKUTree_WJetsPt180_xww.root
 
 #ln -s mu_out_BulkGravWW600.root mu_PKUTree_BulkGravWW600.root
 #ln -s mu_out_BulkGravWW700.root mu_PKUTree_BulkGravWW700.root
