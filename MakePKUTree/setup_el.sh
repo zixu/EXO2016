cca50c75e001abc446dea9e1403365ebbe0ba1e9
zixu <zixu@cern.ch>
2016-01-12 21:31:52 -0600

    allbkg

--- MakePKUTree/setup_el.sh
+++ MakePKUTree/setup_el.sh
@@ -7,7 +7,7 @@ hadd -f el_PKUTree_VV_xww.root el_out_WWToLNuQQ_13TeV-powheg.root el_out_WZTo1L1
 
 hadd -f  el_PKUTree_WJetsPt180_xww.root  el_out_WJetsToLNu_HT-100To200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8.root el_out_WJetsToLNu_HT-200To400_TuneCUETP8M1_13TeV-madgraphMLM-pythia8.root el_out_WJetsToLNu_HT-400To600_TuneCUETP8M1_13TeV-madgraphMLM-pythia8.root el_out_WJetsToLNu_HT-600To800_TuneCUETP8M1_13TeV-madgraphMLM-pythia8.root el_out_WJetsToLNu_HT-800To1200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8.root el_out_WJetsToLNu_HT-1200To2500_TuneCUETP8M1_13TeV-madgraphMLM-pythia8.root el_out_WJetsToLNu_HT-2500ToInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8.root
 
-#hadd -f el_PKUTree_allBkg_xww.root  el_PKUTree_SingleTop_xww.root el_PKUTree_TTBARpowheg_xww.root el_PKUTree_VV_xww.root el_PKUTree_WJetsPt180_xww.root
+hadd -f el_PKUTree_allBkg_xww.root  el_PKUTree_SingleTop_xww.root el_PKUTree_TTBARpowheg_xww.root el_PKUTree_VV_xww.root el_PKUTree_WJetsPt180_xww.root
 
 #ln -s el_out_BulkGravWW600.root el_PKUTree_BulkGravWW600.root
 #ln -s el_out_BulkGravWW700.root el_PKUTree_BulkGravWW700.root
