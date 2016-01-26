9595dd4e48bc853fcfe6791205a7b5b4d8db14d9
zixu <zijun.xu@cern.ch>
2016-01-26 16:49:58 +0100

    Jan26

--- MakePKUTree/runEDBR2PKUTree.py
+++ MakePKUTree/runEDBR2PKUTree.py
@@ -54,14 +54,14 @@ if __name__ == '__main__':
             filein =TFile(input_dir+samplename);
             dir1 = filein.Get("treeDumper");
             treein = dir1.Get("EDBRCandidates")
-            print "Open Tree Done"
+            #print "Open Tree Done"
 
             channel="el"
             analyzer_el=EDBR2PKUTree(treein,channel+"_out_"+samplename);
             analyzer_el.Loop(channel, sampleXS, sampleNum, IsData);
             analyzer_el.endJob();
 
-            channel="mu"
-            analyzer_mu=EDBR2PKUTree(treein,channel+"_out_"+samplename);
-            analyzer_mu.Loop(channel, sampleXS, sampleNum, IsData);
-            analyzer_mu.endJob();
+            ##channel="mu"
+            ##analyzer_mu=EDBR2PKUTree(treein,channel+"_out_"+samplename);
+            ##analyzer_mu.Loop(channel, sampleXS, sampleNum, IsData);
+            ##analyzer_mu.endJob();
