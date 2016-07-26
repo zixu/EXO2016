2f4154a0ce05965cbe8c8c1bb013e936a288fc65
zixu <zijun.xu@cern.ch>
2016-07-26 01:56:52 -0400

    quickly test newstyle_B2GWW_doFit_class.py

---
+++ DoFit/quicklytest.sh
@@ -0,0 +1,8 @@
+python newstyle_B2GWW_doFit_class.py mu BulkGravWW750 650 850 40 150 600 1500 ExpN Pow -b -m 1  --category HP --closuretest 0  --realdata 1  --keepblind 0 -w 0 &
+
+python newstyle_B2GWW_doFit_class.py mu BulkGravWW4500 4400 4600 40 150 800 5000 ExpN Pow -b -m 1 --category HP --closuretest 0  --realdata 1  --keepblind 0 -w 1  &
+
+python newstyle_B2GWW_doFit_class.py el BulkGravWW750 650 850 40 150 600 1500 ExpN Pow -b -m 1  --category HP --closuretest 0  --realdata 1  --keepblind 0 -w 0 &
+
+python newstyle_B2GWW_doFit_class.py el BulkGravWW4500 4400 4600 40 150 800 5000 ExpN Pow -b -m 1 --category HP --closuretest 0  --realdata 1  --keepblind 0 -w 1  &
+
